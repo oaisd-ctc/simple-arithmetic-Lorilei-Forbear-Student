93ea7d56564b3ceefb5409bea5bf19335b4e998b
Lorilei-Forbear-Student <207830@student.oaisd.org>
2024-10-14 13:08:44 +0000

    finished addition operation

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        
+        Console.WriteLine("Please provide a number.");
+        string numOneString = Console.ReadLine();
+        float numOne = float.Parse (numOneString);
+        Console.WriteLine("Please provide another number to add to " + numOne + ".");
+        string numTwoString = Console.ReadLine();
+        float numTwo = float.Parse (numTwoString);
+        float sum = (numOne + numTwo);
+        Console.WriteLine("Your sum is " + sum + ".");
     }
 }
